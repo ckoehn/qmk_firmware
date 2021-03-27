7a47fa3af6706927c556ddeed8441e1717d73132
Christian Köhn <christian.koehn@finleap.com>
2021-03-27 21:57:42 +0100

    Enable home row shift

--- keyboards/dztech/dz65rgb/keymaps/ckoehn/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/ckoehn/keymap.c
@@ -36,9 +36,11 @@ const uint32_t PROGMEM unicode_map[] = {
     [DE_SS] = 0x00DF,
 };
 
-#define CK_ESC MT(MOD_LCTL, KC_ESC)
+#define CK_A MT(MOD_LSFT, KC_A)
 #define CK_ENT MT(MOD_RCTL, KC_ENT)
+#define CK_ESC MT(MOD_LCTL, KC_ESC)
 #define CK_MISC MO(_MISC)
+#define CK_SCLN MT(MOD_RSFT, KC_SCLN)
 #define CK_SPC LT(_DEV, KC_SPC)
 
 #define CK_AE XP(DE_AE, DE_AE_CAP)
@@ -50,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_65_ansi(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        CK_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          CK_ENT,  KC_PGUP,
+        CK_ESC,  CK_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CK_SCLN, KC_QUOT,          CK_ENT,  KC_PGUP,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
         KC_LCTL, CK_MISC, KC_LALT,                            CK_SPC,                    CK_MISC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
