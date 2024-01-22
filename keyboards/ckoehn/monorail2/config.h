5087e464018c193e6392ddb481c3b29b083b9808
Christian Köhn <christian@ckoehn.de>
2024-01-22 23:40:34 +0100

    Monorail2: Initial configuration

---
+++ keyboards/ckoehn/monorail2/config.h
@@ -0,0 +1,31 @@
+/*
+Copyright 2021 Noah Kiser (contact@kiserdesigns.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define LED0_PIN GP25
+#define LED1_PIN GP26
+#define LED2_PIN GP27
+
+// How do you define this in info.json? Check in newer QMK versions!
+#define RGB_MATRIX_LED_COUNT 12
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+#endif
